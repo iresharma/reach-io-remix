d13e7872d9c9a9b85c4e9a79e1f08f3eaaf6c439
Iresh Sharma <iresh.sharma8@gmail.com>
2022-09-23 22:40:35 +0530

    :fire: env variable names changed

--- app/services/aws/s3.aws.server.js
+++ app/services/aws/s3.aws.server.js
@@ -2,8 +2,8 @@ import AWS from "aws-sdk";
 const REGION = "ap-south-1"; //Mumbai
 
 const client = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.REACH_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACH_AWS_SECRET_ACCESS_KEY,
   apiVersion: '2006-03-01',
   region: REGION,
 });
